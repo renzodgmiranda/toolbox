8f56c41607ac2863b804f444eb6ea7d8d11c0e0b
Renzo Miranda <renzo.miranda@teamspan.com>
2023-09-01 13:43:19 +0000

    Minor changes to User schema and ran composer update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('user_preferred');
+            $table->boolean('user_preferred')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
