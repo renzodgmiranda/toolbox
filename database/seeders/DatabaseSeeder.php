a3f6fadf3f383f78ef3f6733463650bdfe6261c4
Renzo Miranda <renzo.miranda@teamspan.com>
2023-09-01 14:17:23 +0000

    Added seeder values for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Customer;
 use App\Models\Permission;
 use App\Models\Role;
+use App\Models\User;
+use App\Models\Workorder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -52,5 +55,75 @@ class DatabaseSeeder extends Seeder
         $clientRole->givePermissionTo($createPermission);
         $clientRole->givePermissionTo($editPermission);
         $clientRole->givePermissionTo($deletePermission);
+
+        //Create users automatically for testing
+        $user = User::create([
+            'name' => 'Renzo Miranda',
+            'email' => 'renzo.miranda@teamspan.com',
+            'password' => bcrypt('Renzo973!#'),
+            'user_preferred' => false,
+        ]);
+        $user->assignRole('Admin');
+
+        $user2 = User::create([
+            'name' => 'John Doe',
+            'email' => 'john.doe@teamspan.com',
+            'password' => bcrypt('renzo973'),
+            'user_preferred' => true,
+        ]);
+        $user2->assignRole('Vendor');
+
+        $user3 = User::create([
+            'name' => 'Jane Doe',
+            'email' => 'jane.doe@teamspan.com',
+            'password' => bcrypt('renzo973'),
+            'user_preferred' => true,
+        ]);
+        $user3->assignRole('Vendor');
+
+        // Array of company names
+        $companies = [
+            'Starbucks',
+            'Apple',
+            'Microsoft',
+            'Google',
+            'Amazon',
+            'Facebook',
+            'Netflix',
+            'Tesla',
+            'Adobe',
+            'Oracle'
+            // ... add more company names as needed
+        ];
+
+        // Loop through the array and seed each company into the customers table
+        foreach ($companies as $company) {
+            Customer::create([
+                'cus_name' => $company,
+            ]);
+        }
+
+        // Arrays of Sample Data
+        $problems = ['Leaky faucet', 'Broken window', 'Damaged roof', 'Electrical issue', 'Heating malfunction'];
+        $priorities = ['High', 'Medium', 'Low'];
+
+        // Loop to create 10 workorder records with random data
+        for ($i = 0; $i < 10; $i++) {
+            Workorder::create([
+                'customer_id' => rand(1, 10), // Assuming you have customers with IDs between 1 and 50
+                'wo_number' => 'WO' . rand(100000, 999999),
+                'wo_problem' => $problems[array_rand($problems)],
+                'wo_problem_type' => $problems[array_rand($problems)], // Modify as needed
+                'wo_description' => 'Description ' . $i, // Modify as needed
+                'wo_customer_po' => 'PO' . rand(100000, 999999),
+                'wo_asset' => 'Asset ' . $i, // Modify as needed
+                'wo_priority' => $priorities[array_rand($priorities)],
+                'wo_trade' => 'Trade ' . $i, // Modify as needed
+                'wo_category' => 'Category ' . $i, // Modify as needed
+                'wo_tech_nte' => 'Technical note ' . $i, // Modify as needed
+                'wo_schedule' => date('Y-m-d H:i:s', strtotime('+'.rand(1,30).' days')), // Randomly scheduled within the next 30 days
+                'wo_status' => 'Pending',
+            ]);
+        }
     }
 }
